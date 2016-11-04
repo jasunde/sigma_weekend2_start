799e846ac49837571aad17238c69b3c875dbeb3d
jasunde <jlaurits@gmail.com>
2016-11-04 17:14:10 -0500

    People on the DOM

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,33 @@
+var sigmanauts = [],
+    $peopleContainer;
+
 $(document).ready(function(){
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
-        console.log(data);
+        sigmanauts = data.sigmanauts;
+        init();
       }
     });
+
+
 });
+
+function init() {
+  $peopleContainer = $('#peopleContainer');
+
+  peopleToDom();
+}
+
+function peopleToDom() {
+  sigmanauts.forEach(function (person) {
+    var $el = $('<div class="person"></div>');
+
+    $el.append('<h2>' + person.name + '</h2>');
+    $el.append('<p>' + person.git_username + '</p>');
+    $el.append('<p>' + person.shoutout + '</p>');
+
+    $peopleContainer.append($el);
+  })
+}
